a3ea62c48c4e6bfe3723fa3d253c4ceac229a21b
linconviana <linconviana@gmail.com>
2022-05-11 01:39:03 -0300

    criado projeto crud

---
+++ src/main/java/com/curso/testeNovoCrud/TesteNovoCrudApplication.java
@@ -0,0 +1,13 @@
+package com.curso.testeNovoCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteNovoCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteNovoCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/testeNovoCrud/TesteNovoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.testeNovoCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteNovoCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
